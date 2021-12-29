5023c3f2bc183b9228a35ec3cf82f7d7a48e910b
jrfulford <90421808+jrfulford@users.noreply.github.com>
2021-12-28 21:07:16 -0600

    Add files via upload

---
+++ activity 01/StudentInfo.java
@@ -0,0 +1,20 @@
+/**
+*Simple program to print Student Info.
+*
+*Activity 1 part A
+*Name: Jack Ryan Fulford - COMP 1210 - Sections 2.1-2.8
+*Date: August 23, 2021
+*/
+public class StudentInfo
+{
+/**
+*Prints Student Info one time.
+*@param args Command line arguments - not used.
+*/
+   public static void main(String[] args)
+   {
+      System.out.println("Name: Jack Ryan Fulford");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   Highschool PLTW Engineering Course");
+   }
+}

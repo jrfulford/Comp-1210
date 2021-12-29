d21adc442c421e01c7c43f7a8fd8bc40f4952490
jrfulford <90421808+jrfulford@users.noreply.github.com>
2021-12-28 21:14:05 -0600

    Add files via upload

---
+++ project 1/JLetter.java
@@ -0,0 +1,27 @@
+/**
+*Simple program to print J Letter.
+*
+*Project 1 part B
+*Name: Jack Ryan Fulford - COMP 1210 - Sections 2.1-2.8
+*Date: August 26, 2021
+*/
+public class JLetter
+{
+/**
+*Prints J Letter one time.
+*@param args Command line arguments - not used.
+*/
+   public static void main(String[] args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
---
+++ project 1/MyLifeGoals.java
@@ -0,0 +1,28 @@
+/**
+*Simple program to print life goals.
+*
+*Project 1 part A
+*Name: Jack Ryan Fulford - COMP 1210 - Sections 2.1-2.8
+*Date: August 26, 2021
+*/
+public class MyLifeGoals
+{
+/**
+*Prints Name and Life Goals one time.
+*@param args Command line arguments - not used.
+*/
+   public static void main(String[] args)
+   {
+      System.out.println("Jack Fulford");
+      System.out.println();
+      System.out.println("In the short term, I would like to have "
+         + "a good GPA for the year. I would also like to make alot of "
+         + "new friends during my time here in Auburn.");
+      System.out.println("In the medium term, I'd like to graduate highly "
+         + "ranked. I'd also like to have a decent job offer out of college "
+         + "that I hwould hopefully enjoy working at.");
+      System.out.println("In the long term, I hope to have a great family. I "
+         + "want to have a great job, and I want to be happy and "
+         + "satisfied with my life choices and decisions.");
+   }
+}
